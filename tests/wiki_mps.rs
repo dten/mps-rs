669ed46c3c3d4d1fad87fb4e2a6fedc6c983c950
David Hewson <dev@daveid.co.uk>
2017-06-06 21:14:30 +0100

    make tests read file

--- tests/wiki_mps.rs
+++ tests/wiki_mps.rs
@@ -2,29 +2,8 @@ extern crate mps;
 
 #[test]
 fn wiki_mps() {
-    let file = r"NAME          TESTPROB
-ROWS
- N  COST
- L  LIM1
- G  LIM2
- E  MYEQN
-COLUMNS
-    XONE      COST                 1   LIM1                 1
-    XONE      LIM2                 1
-    YTWO      COST                 4   LIM1                 1
-    YTWO      MYEQN               -1
-    ZTHREE    COST                 9   LIM2                 1
-    ZTHREE    MYEQN                1
-RHS
-    RHS1      LIM1                 5   LIM2                10
-    RHS1      MYEQN                7
-BOUNDS
- UP BND1      XONE                 4
- LO BND1      YTWO                -1
- UP BND1      YTWO                 1
-ENDATA";
-
-    let result = mps::parse_fixed(file);
+    let file = std::fs::File::open("tests/wiki.mps").unwrap();
+    let result = mps::read(file);
     let prob = result.expect("should be ok");
     assert_eq!(prob.name, "TESTPROB");
     {
