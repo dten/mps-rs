3f67eeb387ccbe6aff3fba20147186c09360a227
David Hewson <dev@daveid.co.uk>
2017-06-06 21:54:16 +0100

    clippy as dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
 use std::io::{Read, BufRead, BufReader};
 use std::rc::Rc;
 use std::collections::{BTreeMap};
