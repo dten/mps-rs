1a41da3fb0ae04f5c287b5ce5785d305020f9d04
David Hewson <dev@daveid.co.uk>
2018-07-01 17:00:28 +0100

    tell clippy shush

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(feature="clippy", allow(just_underscores_and_digits))]
 
 use std::io::{Read, BufRead, BufReader};
 use std::rc::Rc;
